7909b1bbb2712280ec675e6d5b532577b0440539
pewiii <paulewiii3@gmail.com>
2022-04-05 21:08:00 -0400

    Complete up to Video Player

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,18 @@
-var VideoListEntry = () => (
+var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={item(props).thumbnails.default.url} alt={item(props).thumbnails.title} />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{item(props).title}</div>
+      <div className="video-list-entry-detail">{item(props).description}</div>
     </div>
   </div>
 );
+
+var item = function(props) {
+  return props.video.snippet;
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
